925dfa16c52e73107fcf8594704577838bd54705
Ronald <134448744+AndRonald@users.noreply.github.com>
2026-05-31 22:31:02 -0300

    feat: add experience and level up system

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -9,20 +9,22 @@ enum State {
 
 @export_category("Stats")
 @export var speed: int = 400
-@export var attack_speed: float = 0.6
 @export var attack_damage:int  = 60
 @export var hitpoints = 150
 
 var last_dir: Vector2 = Vector2.RIGHT
 var state: State = State.IDLE
 var move_direction: Vector2 = Vector2(0,0)
+var attack_speed: float
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var animation_playback: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
 
 
 func _ready() -> void:
+	add_to_group("players")
 	animation_tree.set_active(true)
+	calculate_stats()
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -33,6 +35,14 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	if not state == State.ATTACK:
 		movement_loop()
+		
+
+func calculate_stats() -> void:
+	attack_speed = Equations.calculate_attack_speed()
+	var time_factor: float = Equations.BASE_ATTACK_SPEED / attack_speed
+	animation_tree.set("parameters/attack/TimeScale/scale", time_factor)
+	print("my new attack speed", attack_speed)
+
 		
 func movement_loop() -> void:
 		move_direction.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
